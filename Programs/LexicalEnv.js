55841f5ba90e617f91fdfd14becdad068e7de243
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-09-28 23:58:19 +0530

    Lexical Environment program

---
+++ Programs/LexicalEnv.js
@@ -0,0 +1,13 @@
+
+//Whenever a execution is created a Lexical environment is also created
+
+var a = 10;
+
+function hello1(){
+
+        
+        function sbc (){
+        console.log(a);
+    
+    }
+}
